53c32eaa9d62959ba212b6a8a909369f126fb51a
ilyhus001 <127707438+ilyhus001@users.noreply.github.com>
2023-09-24 21:45:39 -0400

    Update AddressBook.java
    Changed completion message

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -25,7 +25,7 @@ public class AddressBook {
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(0);
-        System.out.println("Complete!");
+        System.out.println("Completed Lab 3!");
     }
 
 }
